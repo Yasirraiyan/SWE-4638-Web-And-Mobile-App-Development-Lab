b4e45ea9e9e8e7ccfae1fd32c45e865521a40aaf
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-06-24 20:55:14 +0600

    Create HeroComponent.js

---
+++ Lab-02/Task-02/HeroComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HeroComponent() 
+{
+  return 
+    (
+    <div className="hero">
+      <h1>Modern Strategy</h1>
+      <p>Ultimate Marketing to help you grow your business</p>
+      <button>Let’s discuss your project</button>
+    </div>
+  );
+}
+
+export default HeroComponent;
