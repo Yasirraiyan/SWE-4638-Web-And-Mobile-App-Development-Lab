143eac67376ebdaa4bce014cdb37312c55a94614
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-06-24 20:54:35 +0600

    Create NavComponent.js

---
+++ Lab-02/Task-02/NavComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavComponent() {
+  return (
+    <nav className="nav">
+      <h2>Softy Pinko</h2>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavComponent;
